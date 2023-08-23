ef95ebdf22c675c5406988fe59e92732e9b7cf70
bris0231 <54876624+bris0231@users.noreply.github.com>
2023-08-23 15:22:37 -0400

    course update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'MAD9011',
-  description: 'Mobile Design Fundamentals',
-  base: '/f2023/', //used as the root location of the site (repo name)
+  title: "MAD9011",
+  description: "Mobile Design Fundamentals",
+  base: "/f2023/", //used as the root location of the site (repo name)
   // then we can <img :src="$withBase('/foo.png')" alt="foo">
   markdown: {
     lineNumbers: true,
@@ -9,8 +9,8 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': 'path/to/some/dir',
-        '@root': '/f2023',
+        "@alias": "path/to/some/dir",
+        "@root": "/f2023",
         //![Image from alias](~@alias/image.png)
         // ![Image alt text](~@root/modules/week3/myimg.jpg)
         // images inside /docs/.vuepress/public will be copied to repo root
@@ -19,73 +19,69 @@ module.exports = {
   },
   // themeEntry or theme?: '@vuepress/theme-default',
   plugins: [
-    '@vuepress/back-to-top',
+    "@vuepress/back-to-top",
     [
-      '@vuepress/search',
+      "@vuepress/search",
       {
         searchMaxSuggestions: 10,
       },
     ],
-    '@vuepress/last-updated',
+    "@vuepress/last-updated",
   ],
   themeConfig: {
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
     //displayAllHeaders: true,
     nextLinks: true,
     prevLinks: true,
     serviceWorker: {
       updatePopup: {
-        message: 'New content is available.',
-        buttonText: 'Refresh',
+        message: "New content is available.",
+        buttonText: "Refresh",
       },
     },
     nav: [
       {
-        text: 'Overview',
-        link: '/overview/',
+        text: "Overview",
+        link: "/overview/",
       },
       {
-        text: 'Deliverables',
-        link: '/deliverables/',
+        text: "Deliverables",
+        link: "/deliverables/",
       },
       {
-        text: 'Modules',
-        link: '/modules/gettingstarted/',
-      },
-      {
-        text: 'Resources',
-        link: '/resources/',
+        text: "Modules",
+        link: "/modules/gettingstarted/",
       },
     ],
     sidebar: [
       {
-        title: 'Content Modules',
+        title: "Content Modules",
         collapsable: true,
         sidebarDepth: 2,
         children: [
-          '/modules/gettingstarted/',
-          '/modules/week1/',
-          '/modules/week2/',
-          '/modules/week3/',
-          '/modules/week4/',
-          '/modules/week5/',
-          '/modules/week6/',
-          '/modules/week7/',
-          '/modules/week8/',
-          '/modules/week9.1/',
-          '/modules/week9.2/',
-          '/modules/week10/',
-          '/modules/week11/',
-          '/modules/week12/',
-          '/modules/week13/',
-          '/modules/week14/'
+          "/modules/gettingstarted/",
+          "/modules/week1/",
+          "/modules/week2/",
+          "/modules/week3/",
+          "/modules/week4/",
+          "/modules/week5/",
+          "/modules/week6/",
+          "/modules/week7/",
+          "/modules/week8/",
+          "/modules/week9.1/",
+          "/modules/week9.2/",
+          "/modules/week10/",
+          "/modules/week11/",
+          "/modules/week12/",
+          "/modules/week13/",
+          "/modules/week14/",
         ],
       },
       {
-        title: 'Deliverables',
+        title: "Deliverables",
         collapsable: true,
         sidebarDepth: 2,
-        children: ['/deliverables/'],
+        children: ["/deliverables/"],
       },
     ],
   },
