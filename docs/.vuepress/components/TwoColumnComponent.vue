7abc73ea1987ca0fdc28f3ba283580fb523cd86f
bris0231 <54876624+bris0231@users.noreply.github.com>
2023-10-16 23:17:39 -0400

    course update

---
+++ docs/.vuepress/components/TwoColumnComponent.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="two-column-component">
+    <div class="first-column">
+      <img :src="imageSrc" :style="imageStyle" alt="Image" />
+    </div>
+    <div class="column">
+      <h4>{{ title }}</h4>
+      <p>{{ description }}</p>
+      <a :href="linkUrl">{{ linkText }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageSrc: String,
+    title: String,
+    description: String,
+    linkUrl: String,
+    linkText: String,
+    imageSize: {
+      type: String,
+      default: "100%", // Default to 100% size if not provided
+    },
+  },
+  computed: {
+    imageStyle() {
+      return {
+        "max-width": this.imageSize,
+        "max-height": this.imageSize,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.two-column-component {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #eaecef;
+}
+
+.column,
+.first-column {
+  flex: 1;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.first-column {
+  max-width: fit-content;
+  align-items: center;
+}
+
+a {
+  color: #3eaf7c;
+  text-decoration: underline;
+}
+
+h4 {
+  margin: 0;
+}
+</style>
---
+++ docs/.vuepress/components/VideoCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="video-card" :style="borderStyle">
+    <div class="column">
+      <slot></slot>
+    </div>
+    <div class="video-column">
+      <iframe
+        :src="youtubeUrl"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    youtubeTitle: String,
+    youtubeUrl: {
+      type: String,
+      default: "https://www.youtube.com/embed/TesDIVA28rM",
+    },
+    isFirst: Boolean, // Add a prop to indicate if it's the first in the list
+  },
+  computed: {
+    borderStyle() {
+      return this.isFirst
+        ? "border-top: none;"
+        : "border-top: 1px solid #eaecef;";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.video-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center; /* Vertically center the content */
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+.column,
+.video-column {
+  flex: 1;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.video-column {
+  max-width: 250px;
+  height: auto;
+}
+
+iframe {
+  max-width: 100%;
+  height: auto;
+}
+</style>
